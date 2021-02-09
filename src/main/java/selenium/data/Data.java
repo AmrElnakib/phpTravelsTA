299dec124d6ee56d9aa54686c11e82c3bb0d3a4e
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-02-09 12:30:52 +0200

    add data class

---
+++ src/main/java/selenium/data/Data.java
@@ -0,0 +1,22 @@
+package selenium.data;
+
+public class Data {
+
+    // ====| ADMIN |===
+
+    public String adminUrl = "https://www.phptravels.net/admin";
+    public String correctAdminEmail = "admin@phptravels.com";
+    public String correctAdminPassword = "demoadmin";
+    public String incorrectAdminEmail = "incorrectadmin@phptravels.com";
+    public String incorrectAdminPassword = "incorrectdemoadmin";
+
+    // ====| FRONT PAGE |===
+
+    public String frontPageUrl = "https://www.phptravels.net/";
+    public String frontLoginPageUrl = "https://www.phptravels.net/login";
+    public String correctFrontPageEmail = "user@phptravels.com";
+    public String correctFrontPagePassword = "demouser";
+    public String incorrectFrontPageEmail = "wrong@phptravels.com";
+    public String incorrectFrontPagePassword = "incorrectdemoadmin";
+
+}
